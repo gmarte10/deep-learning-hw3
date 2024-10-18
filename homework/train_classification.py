3919a9ee7f3c3385b55c1a19441c0061c798bbee
gmarte10 <giancarlosmarte10@gmail.com>
2024-10-18 14:06:34 -0400

    fixed tensor dict issue

--- homework/train_classification.py
+++ homework/train_classification.py
@@ -102,8 +102,8 @@ def train_classification(
             acc_storage["validation_accuracy"].append(acc_metric.compute())
         
         # Get the mean training and validation accuracy for the epoch
-        epoch_train_acc = torch.as_tensor(acc_storage["train_accuracy"]).mean()
-        epoch_val_acc = torch.as_tensor(acc_storage["validation_accuracy"]).mean()
+        epoch_train_acc = torch.tensor(acc_storage["train_accuracy"]).float().mean()
+        epoch_val_acc = torch.tensor(acc_storage["validation_accuracy"]).float().mean()
 
         # Log the training and validation accuracy
         logger.add_scalar("train_accuracy", epoch_train_acc, global_step)
