78a238124313bbb5bcedc524760d651ab33346a4
gmarte10 <giancarlosmarte10@gmail.com>
2024-10-23 14:42:15 -0400

    decreased validation

--- homework/datasets/classification_dataset.py
+++ homework/datasets/classification_dataset.py
@@ -42,7 +42,7 @@ class SuperTuxDataset(Dataset):
                     transforms.RandomResizedCrop(size=(64, 64), antialias=True),
                     transforms.RandomHorizontalFlip(),
                     transforms.RandomRotation(degrees=15),
-                    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+                    # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
                     transforms.ToTensor(),
                 ]
             )
