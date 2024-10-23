b92330f0e8448e70d4c4196e6658dd026fe710c3
gmarte10 <giancarlosmarte10@gmail.com>
2024-10-23 18:57:23 -0400

    changed default classification

--- homework/train_classification.py
+++ homework/train_classification.py
@@ -16,8 +16,8 @@ def train_classification(
         model_name: str = "classification",
         num_epoch: int = 50,
         # Learning rate for the optimizer
-        lr: float = 0.00005,
-        batch_size: int = 128,
+        lr: float = 0.0001,
+        batch_size: int = 64,
         # Random seed for reproducibility
         seed: int = 2024,
         # Additional keyword arguments to pass to the model
